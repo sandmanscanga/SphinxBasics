9574614000170413666cc5ca5ce10d0ff73f71b2
Adam Scanga <716-AS0549@users.noreply.gitlab.mycompanyview.com>
2024-05-09 06:56:39 -0400

    Moved code back to google style docstrings

--- serenity/src/serenity/actions.py
+++ serenity/src/serenity/actions.py
@@ -1,13 +1,59 @@
-# serenity.actions.py
-"""Actions that can be performed with Firefly and its crew."""
+"""
+Module grouping actions that can be performed with Firefly.
 
-def change_pilot(firefly, pilot):
-    """Changes the pilot on a :class:`serenity.ship.Firefly` instance to the
+Summary:
+    This module contains functions that can be used to perform
+    actions with a Firefly and its crew. The functions are simple
+    and are intended to demonstrate how to document functions in a
+    Python package using Sphinx and ReadTheDocs.
+
+Functions:
+    change_pilot: Changes the pilot on a Firefly instance to the
     given value.
 
-    :param firefly: :class:`Firefly <serenity.ship.Firefly>` instance who's
-        :attr:`~.ship.Firefly.pilot` attribute is to be changed
-    :param pilot: String with the new pilot's name
+Example:
+    The following example demonstrates how to use the functions in
+    this module:
+
+    >>> from serenity.ship import Firefly
+    >>> from serenity.actions import change_pilot
+    >>> firefly = Firefly()
+    >>> change_pilot(firefly, "Malcolm Reynolds")
+    >>> print(firefly.pilot)
+    Malcolm Reynolds
+
+"""
+
+from serenity.ship import Firefly
+
+
+def change_pilot(firefly: Firefly, pilot: str) -> None:
     """
-    # This is a silly example that really should be a method on the object
+    Changes the pilot on a Firefly instance to the given value.
+
+    Summary:
+        This function changes the pilot on a Firefly instance to the
+        given value. The function takes a Firefly instance and a
+        string with the new pilot's name as arguments. The function
+        then sets the pilot attribute of the Firefly instance to the
+        given value.
+
+    Args:
+        firefly (Firefly): The Firefly instance who's pilot
+        attribute is to be changed.
+        pilot (str): The new pilot's name.
+
+    Example:
+        The following example demonstrates how to use the function:
+
+        >>> from serenity.ship import Firefly
+        >>> from serenity.actions import change_pilot
+        >>> firefly = Firefly()
+        >>> change_pilot(firefly, "Malcolm Reynolds")
+        >>> print(firefly.pilot)
+        Malcolm Reynolds
+
+    """
+
+    # This is a silly example that should be a method on the object
     firefly.pilot = pilot
